eceb9650a01e41b88384ed8c1d163e017ae4ead8
abraham23o <abraonyonka23@gmail.com>
2023-11-28 16:46:37 +0300

    refactor print last digit task

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ sign = -1 if number < 0 else 1
 last_digit = int(str(number)[-1]) * sign
 
 if last_digit > 5:
-    txt = 'and is less than 5'
+    txt = 'and is greater than 5'
 elif last_digit == 0:
     txt = 'and is 0'
 else:
     txt = 'and is less than 6 and not 0'
 
-print(f"Last digit of {number} is {last_digit} {txt}") 
+print(f"Last digit of {number} is {last_digit} {txt}")
