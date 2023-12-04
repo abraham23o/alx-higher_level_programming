df85889fe68499fa4c51a315608865fea8811cc6
abraham23o <abraonyonka23@gmail.com>
2023-12-04 22:36:12 +0300

    refactor prints all integers of a list, in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-        for num in list(my_list[::-1]):
-            print("{:d}".format(num))
+    if not my_list:
+        return
+    for num in list(my_list[::-1]):
+        print("{:d}".format(num))
