6cf33d7ce8bf5b60509a7e6580fe5f0a443b1d57
abraham23o <abraonyonka23@gmail.com>
2023-12-04 22:57:35 +0300

    refactor replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if not my_list: 
+    if my_list is None: 
         return
     lst = my_list.copy()
     if idx > len(lst) - 1 or idx < 0:
