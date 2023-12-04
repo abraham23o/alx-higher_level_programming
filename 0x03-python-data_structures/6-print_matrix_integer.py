57bc874331ae1f3bfe58fb1ffa80e9b91bba9ad3
abraham23o <abraonyonka23@gmail.com>
2023-12-04 23:30:56 +0300

    refactor print matrix integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elem in row:
-            print(elem, end=" ")
+        for elem in range(len(row)):
+            if elem == len(row) - 1:
+                print(row[elem], end="")
+            else:
+                print(row[elem], end=" ")
         print()
