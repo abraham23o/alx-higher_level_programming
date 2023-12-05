54b2d4b1ebc3c51ea6350ec563c259a20e170137
abraham23o <abraonyonka23@gmail.com>
2023-12-05 18:38:36 +0300

    refactor commandline args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    args = len(sys.argv) - 1
-    if args == 1:
-        print("{:d} arguments:".format(args))
-    for i in range(1, args + 1):
-            print("{0}: {1}".format(i, sys.argv[i]))
+    from sys import argv
+    if len(argv[1:]) == 1:
+        print("{} argument:".format(len(argv[1:])))
+    elif len(argv[1:]) == 0:
+        print("{} arguments.".format(len(argv[1:])))
+    else:
+        print("{} arguments:".format(len(argv[1:])))
+    for i in argv[1:]:
+        print("{}: {}".format((argv[1:].index(i)) + 1, i))
