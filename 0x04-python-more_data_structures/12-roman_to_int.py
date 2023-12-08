897ead77b62e317a98d3d2c7ca842600f9e9b0a1
abraham23o <abraonyonka23@gmail.com>
2023-12-08 16:59:36 +0300

    refactor convert roman numeral to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,20 +16,3 @@ def roman_to_int(roman_string):
         else:
             return 0
     return res
-
-    # for i in roman_string:
-    #     if i == 'M':
-    #         res += 1000
-    #     elif i == 'D':
-    #         res += 500
-    #     elif i == 'C':
-    #         res += 100
-    #     elif i == 'L':
-    #         res += 50
-    #     elif i == 'X':
-    #         res += 10
-    #     elif i == 'V':
-    #         res += 5
-    #     elif i == 'I':
-    #         res += 1
-    # return res
