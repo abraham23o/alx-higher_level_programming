596ffa8dc4ebd465599be644f3e0c7e08a0bb212
abraham23o <abraonyonka23@gmail.com>
2023-12-20 09:27:45 +0300

    square class continuation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class Definition continuation"""
+
+
+class Square:
+    """Square class definition - Initialize size with optional val"""
+
+    def __init__(self, size=0):
+        self.__size = size
