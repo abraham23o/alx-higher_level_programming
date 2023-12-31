910ee445d13ef8cf1967e1bdf9317c97c9aba4ca
abraham23o <abraonyonka23@gmail.com>
2023-12-31 23:40:47 +0300

    refactor print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for num in my_list[:x]:
         try:
-            print(num, end="")
+            print("{:d}".format(num), end="")
             count += 1
 
         except IndexError:
