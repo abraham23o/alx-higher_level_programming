6782e453b0396baeee34154b2c61fa8f5c21bfee
abraham23o <abraonyonka23@gmail.com>
2024-01-04 22:16:40 +0300

    refactor real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,10 +28,10 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
