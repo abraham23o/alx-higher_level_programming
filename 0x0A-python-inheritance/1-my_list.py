6fbd0c8dedf53be4cd1a8b381e94a863b156ffb4
abraham23o <abraonyonka23@gmail.com>
2024-01-08 21:50:08 +0300

    print sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+prints a list in sorted order
+"""
+
+
+class MyList(list):
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        prints a list in sorted order
+        """
+        sorted_list = self[:]
+        sorted_list.sort()
+        print(sorted_list)
+
