92136f07c12cdf7a04063f23dda9bffc6262e599
abraham23o <abraonyonka23@gmail.com>
2024-01-10 13:50:28 +0300

    refactor script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,21 +6,21 @@ list, and then save them to a file:
 You must use your function save_to_json_file from 5-save_to_json_file.py
 You must use your function load_from_json_file from 6-load_from_json_file.py
 The list must be saved as a JSON representation in a file named add_item.json
-If the file doesn’t exist, it should be created
+If the file does not exist, it should be created
 You don’t need to manage file permissions / exceptions.
 """
 import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+    filename = "add_item.json"
 
-try:
-    data = load_from_json_file(filename)
-except FileNotFoundError:
-    data = []
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
 
-for i in range(1, len(sys.argv)):
-    data.append(sys.argv[i])
-    save_to_json_file(data, filename)
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, filename)
