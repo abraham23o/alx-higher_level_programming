a099e043ca833f6d99a373bb875f31190d123b7e
abraham23o <abraonyonka23@gmail.com>
2024-01-10 04:40:00 +0300

    read a file and print its contents

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that reads a file and outputs
+it to the stdout
+"""
+
+
+def read_file(filename=""):
+    """reads and prints out content of a file"""
+    with open(filename, encoding="utf-8") as f:
+        data_read = f.read()
+        print(data_read)
