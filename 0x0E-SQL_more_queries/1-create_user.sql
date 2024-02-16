ab1a10c27c363c7c8c4010448c5a254f6fdd1ebe
abraham23o <abraonyonka23@gmail.com>
2024-02-16 18:07:02 +0300

    script to create user and grant him all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED
-WITH mysql_native_password BY 'user_0d_1_Pwd';
+BY 'user_0d_1_Pwd';
 
 -- give user user_0d_1 all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
