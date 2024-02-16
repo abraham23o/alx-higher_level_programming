4e72a5e64f6aa50c15dd797fc36f5b0ac84e6f09
abraham23o <abraonyonka23@gmail.com>
2024-02-16 18:32:42 +0300

    create a read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- create a database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- create user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED
+WITH mysql_native_password BY 'user_0d_2_Pwd';
+
+-- grant SELECT privilege to user_0d_2 in database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
