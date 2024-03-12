f5e0fede9466d0a814f2dfc1bfaed7e9b1560f46
abraham23o <abraonyonka23@gmail.com>
2024-03-12 08:29:48 +0300

    First constant, first print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+//creating a constant variable myVar with value "Javascript is amazing"
+const myVar = "JavaScript is amazing";
+
+//printout the value of myVar using console.log
+console.log(myVar);
