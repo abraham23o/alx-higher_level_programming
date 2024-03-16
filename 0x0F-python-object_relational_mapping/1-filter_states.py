76491764248185e96616d8a60f2effa287564436
abraham23o <abraonyonka23@gmail.com>
2024-03-16 20:37:52 +0300

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module lists all states in a database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    for i in cursor.fetchall():
+        print(i)
+
+    cursor.close()
+    connection.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module lists all states in a database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+
+    query = """SELECT * FROM states WHERE name LIKE BINARY
+     '{}' ORDER BY id ASC""".format(sys.argv[4])
+
+    cursor.execute(query)
+
+    for i in cursor.fetchall():
+        print(i)
+
+    cursor.close()
+    connection.close()
