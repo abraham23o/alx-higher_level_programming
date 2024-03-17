234cfa7ddb938789a4dcbb10d09e3c07bd1761f6
abraham23o <abraonyonka23@gmail.com>
2024-03-17 09:46:16 +0300

    All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module lists all cities of state passed as
+an argument in a database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+
+    query = """SELECT c.name
+     FROM states s INNER JOIN cities c ON s.id = c.state_id 
+     WHERE s.name = %s ORDER BY c.id ASC"""
+
+    cursor.execute(query, (sys.argv[4], ))
+
+    attributes = cursor.fetchall()
+    print(", ".join([city[0] for city in attributes]))
+
+    cursor.close()
+    connection.close()
