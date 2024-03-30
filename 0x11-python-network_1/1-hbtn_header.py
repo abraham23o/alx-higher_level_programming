38d7a8420f1e236636de10801ca77f910df71656
abraham23o <abraonyonka23@gmail.com>
2024-03-31 00:03:28 +0300

    takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -20,7 +20,13 @@ import sys
 
 url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    # Retrieve and display the value of the X-Request-Id header
-    x_request_id = response.getheader('X-Request-Id')
-    print(x_request_id)
+try:
+    with urllib.request.urlopen(url) as response:
+        # Retrieve and display the value of the X-Request-Id header
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)
+
+except urllib.error.URLError as e:
+    print(f"Error accessing the URL: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
