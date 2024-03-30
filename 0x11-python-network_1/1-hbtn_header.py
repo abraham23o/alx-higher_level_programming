641fc53b82865b11fa0d1dd76c693ab19b9b0e58
abraham23o <abraonyonka23@gmail.com>
2024-03-30 22:55:04 +0300

    takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -18,20 +18,9 @@ found in the header of the response"""
 import urllib.request
 import sys
 
-# Check if a URL is provided as a command-line argument
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
-
 url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        # Retrieve and display the value of the X-Request-Id header
-        x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
-
-except urllib.error.URLError as e:
-    print(f"Error accessing the URL: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+with urllib.request.urlopen(url) as response:
+    # Retrieve and display the value of the X-Request-Id header
+    x_request_id = response.getheader('X-Request-Id')
+    print(x_request_id)
