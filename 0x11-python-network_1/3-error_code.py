1bb16edab91411997a38eed1a5c0900df241a0fb
abraham23o <abraonyonka23@gmail.com>
2024-03-30 23:37:14 -0700

    send request to a url, display response, handle http error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,27 +3,15 @@
 This script takes in a URL, sends a request to the URL and
 displays the body of the response
 """
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
 
-import urllib.error
-import urllib.request
-import sys
 
-# Check if a URL is provided as a command-line argument
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
-
-url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        # Retrieve and display the body of the response
-        response_body = response.read().decode('utf-8')
-        print(response_body)
-
-except urllib.error.HTTPError as e:
-    print(f"Error Code: {e.code}")
-except urllib.error.URLError as e:
-    print(f"Error accessing the URL: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
