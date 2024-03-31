f44e04882dfeda82a61b2561830b10a1b300d127
abraham23o <abraonyonka23@gmail.com>
2024-03-30 23:04:17 -0700

    takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body
+of the response (decoded in utf-8)
+"""
+
+import urllib.request
+import sys
+import urllib.parse
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+try:
+    with urllib.request.urlopen(url, data=data) as response:
+        resp_body = response.read().decode('utf-8')
+        print(resp_body)
+
+except urllib.error.URLError as e:
+    print(f"Error accessing the URL: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
