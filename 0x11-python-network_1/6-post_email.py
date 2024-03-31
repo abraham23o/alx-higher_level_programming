0a0c1c2c41c0166593cf6da7e5e9a37605186696
abraham23o <abraonyonka23@gmail.com>
2024-03-31 12:41:35 -0700

    sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,21 +9,9 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    if len(argv) != 3:
-        print("Usage: python script.py <URL> <email>")
-        exit(1)
     url = argv[1]
     email = argv[2]
 
-    data = {'email': email}
-
-    try:
-        # Send a POST request with the email parameter
-        response = requests.post(url, data=data)
-        print(response.text)
-
-    except requests.RequestException as e:
-        print(f"Error accessing the URL: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
+    data = {'email':email}
+    response = requests.post(url, data=data)
+    print(response.text)
