ecf074582786154b7c452c0572cd77e09c17df28
abraham23o <abraonyonka23@gmail.com>
2024-04-24 05:24:04 +0300

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -9,10 +9,10 @@ if (process.argv.length !== 3) {
 
 const url = process.argv[2];
 
-request.get(url, (err, response) => {
-  if (err) {
-    console.error(err);
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
