84f2860188253763e7b7d26a17ba30ebf19540e9
abraham23o <abraonyonka23@gmail.com>
2024-05-07 20:20:25 +0300

    Add  class and Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('green')) {
+      $('header').removeClass('green').addClass('red');
+    }
+    else if ($('header').hasClass('red')) {
+     $('header').removeClass('red').addClass('green');
+    }
+    else {
+      $('header').addClass('red');
+    }
+  });
+});
